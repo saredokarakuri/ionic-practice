41e6723c435cb1bfdac4d0033ae827a569440d8b
saredokarakuri <taxing.iota.0r@icloud.com>
2022-11-17 16:47:22 +0900

    Change from typescript to javascript

--- ionic-vue/src/router/index.js
+++ ionic-vue/src/router/index.js
@@ -1,17 +1,9 @@
 import { createRouter, createWebHistory } from '@ionic/vue-router';
-import { RouteRecordRaw } from 'vue-router';
 import HomePage from '../views/HomePage.vue'
 
-const routes: Array<RouteRecordRaw> = [
-  {
-    path: '/',
-    redirect: '/home'
-  },
-  {
-    path: '/home',
-    name: 'Home',
-    component: HomePage
-  }
+const routes = [
+  { path: '/', redirect: '/home' },
+  { path: '/home', name: 'Home', component: HomePage }
 ]
 
 const router = createRouter({
